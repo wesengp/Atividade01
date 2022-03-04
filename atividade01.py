d0f2f124e0b008ab0d5b660dbafbab12ec5a270c
Wesley Fonseca <Wesleyfonsecaengp@gmail.com>
2022-03-04 00:37:56 -0300

    Questão 1

---
+++ atividade01.py
@@ -0,0 +1,10 @@
+print('-'*40)
+p1 = str(input('digite seu nome: '))
+p2 = str(input('digite seu nome: '))
+p3 = str(input('digite seu nome: '))
+t1 = int(input('Digite seu telefone: '))
+t2 = int(input('Digite seu telefone: '))
+t3 = int(input('Digite seu telefone: '))
+print('nome:{} telefone:{}\n nome:{} telefone:{}\n nome:{} telefone:{}'.format(p1,t1,p2,t2,p3,t3) )
+
+print('-'*40)
